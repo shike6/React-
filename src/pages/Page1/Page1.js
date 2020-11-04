056a4769f0d6cb1184bf801b7d46dbf6f4ed7b31
shike <934458241@qq.com>
2020-11-04 16:28:40 +0800

    实现webpack-dev-server,热更新，文件路径优化配置

---
+++ src/pages/Page1/Page1.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+export default class Page1 extends Component {
+    render() {
+        return (
+            <div>this is Page1~</div>
+        )
+    }
+}
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,7 +1,10 @@
 const path = require('path');
 module.exports = {
     /*入口*/
-    entry: path.join(__dirname, 'src/index.js'),
+    entry: [
+        'react-hot-loader/patch',
+        path.join(__dirname, 'src/index.js')
+    ],
     /*输出到dist文件夹，输出文件名字为bundle.js*/
     output: {
         path: path.join(__dirname, './dist'),
@@ -15,6 +18,20 @@ module.exports = {
             use: ['babel-loader?cacheDirectory=true'],
             include: path.join(__dirname, 'src')
         }]
+    },
+    devServer: {
+        port: 8080,
+        contentBase: path.join(__dirname, './dist'),
+        historyApiFallback: true,
+        host: '0.0.0.0'
+    },
+    /*文件路径优化*/ 
+    resolve: {
+        alias: {
+            pages: path.join(__dirname, 'src/pages'),
+            component: path.join(__dirname, 'src/component'),
+            router: path.join(__dirname, 'src/router')
+        }
     }
 
 };
